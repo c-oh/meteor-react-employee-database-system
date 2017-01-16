ce501f1c8be3be0993c8cbd6fc7cdc66486a5043
hollythedev <papiniu@outlook.com>
2017-01-16 15:42:50 -0800

    Created a new employee detail component and imported it

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {Employees} from '../../imports/collections/employees';
+import {EmployeeDetail} from './employee_detail';
+import {createContainer} from 'meteor/react-meteor-data';
 
-const EmployeeList = () =>{
+
+const EmployeeList = (props) =>{
+    //props.employees => an array of employee objects
+
+
     return(
         <div>
         <div className="employee-list">
-        <p> Employee list </p>
+
         </div>
         </div>
     )
 }
 
-export default EmployeeList;
+export default createContainer(() => {
+
+//sets up the subscription of meteor, to return the object and 
+Meteor.subscribe('employees');
+
+
+//whatever is returend will be sent to Employeelist as a prop
+//.fetch excecutes the search, and only fetches records we have subscribed to "employees"
+
+return {employees: Employees.find({}).fetch()};
+
+
+},EmployeeList);
